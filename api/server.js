6c730de7e7cb00184f24e08d58665866ed1132ce
david fletcher <davidfletcheraaron@gmail.com>
2022-01-24 20:27:01 -0500

    had my dang routers in the wrong place

--- api/server.js
+++ api/server.js
@@ -20,12 +20,13 @@ const usersRouter = require('./users/users-router')
 
 const server = express();
 
-server.use('/api/auth', authRouter)
-server.use('/api/users', usersRouter)
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/auth', authRouter)
+server.use('/api/users', usersRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
